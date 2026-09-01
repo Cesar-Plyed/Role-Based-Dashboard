4e17496b1978a820b9e4f140a24a91e5f7bc4b42
Cesar Eduardo <cesar.plyed@gmail.com>
2026-09-01 17:26:52 -0600

    feat: adder more enviroments endpoints and prepare for recibe user carts al list products

--- frontend/src/enviroments/enviroment.ts
+++ frontend/src/enviroments/enviroment.ts
@@ -3,6 +3,15 @@ export const enviroment = {
   apiUrl: 'http://localhost:8080/api',
   authEndpoints: {
     login: '/auth/login',
-    register: '/auth/register'
+    register: '/auth/register',
+  },
+  productsEndpoints: {
+    products: '/products',
+  },
+  cartEndpoints: {
+    userCart: (userId: string | number) => `/cart/${userId}`,
+    addItemsInUserCart: (userId: string | number) => `/cart/${userId}/items`,
+    removeUserItem: (userId: string | number, cartItem: string | number) =>
+      `/cart/${userId}/items/${cartItem}`,
   },
 };
